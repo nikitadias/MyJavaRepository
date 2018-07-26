148c08cb9bc4e82c011878d0271e104fd2c80a60
Nikita Dias <nikita_dias@persistent.co.in>
2018-07-26 14:54:52 +0530

    displayDate methid improved

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,11 +1,10 @@
 class HelloWorld{
 
-void displayDate(){
-sysout("Display today's date:-");
-}
-		public static void main(String[] args)
-			{   
-				System.out.println("HelloWorld");
-				System.out.println("Welcome to GIT");
-}
+	void displayDate(){
+		System.out.println("Display today's date:-");
+	}
+	public static void main(String[] args){   
+		System.out.println("HelloWorld");
+		System.out.println("Welcome to GIT");
+	}
 }

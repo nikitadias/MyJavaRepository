f3c570268de7d92fda02ccb9d7fb283a25b8fe8d
Nikita Dias <nikita_dias@persistent.co.in>
2018-07-26 15:08:55 +0530

    calling static displayDate method

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,10 +1,11 @@
 class HelloWorld{
 
-	void displayDate(){
+	static void displayDate(){
 		System.out.println("Display today's date:-");
 	}
 	public static void main(String[] args){   
 		System.out.println("HelloWorld");
 		System.out.println("Welcome to GIT");
+		displayDate();
 	}
 }
